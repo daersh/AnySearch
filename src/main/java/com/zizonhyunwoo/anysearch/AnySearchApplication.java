7e277f70fc4836895f5d36c64138146a9128dfc0
hyunwoo <hyunwoo@wisenut.co.kr>
2025-07-01 14:18:56 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/zizonhyunwoo/anysearch/AnySearchApplication.java
@@ -0,0 +1,13 @@
+package com.zizonhyunwoo.anysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnySearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnySearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zizonhyunwoo/anysearch/AnySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zizonhyunwoo.anysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
