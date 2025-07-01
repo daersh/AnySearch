4d3c53e007cffd40cd6fcf3125afb7ac3b89b908
hyunwoo <hyunwoo@wisenut.co.kr>
2025-07-01 17:01:07 +0900

    test: jwt filter test
    - 요청 토큰 처리하기 위한 jwt filter 로직 테스트

---
+++ src/test/java/com/zizonhyunwoo/anysearch/util/JwtFilterTest.java
@@ -0,0 +1,41 @@
+package com.zizonhyunwoo.anysearch.util;
+
+import com.zizonhyunwoo.anysearch.domain.UserInfo;
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class JwtFilterTest {
+    String token;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    UserInfo userInfo = new UserInfo("name","password","email","nickname","ROLE_USER");
+
+    @BeforeEach
+    public void init(){
+        token = jwtUtil.createJwt(userInfo,"access");
+    }
+
+    @Test
+    @DisplayName("jwt 토큰 필터 통과")
+    public void jwt_필터_통과(){
+
+        assertNotNull(token);
+        Claims claims = jwtUtil.parseJwt(token.substring(7));
+
+        assertFalse(claims.getExpiration().before(new Date(System.currentTimeMillis())));
+        assertEquals("access", claims.get("type", String.class));
+
+    }
+
+
+
+}
