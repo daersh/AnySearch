01072b3998782499d3cd63ab5f80935f8498a5bd
hyunwoo <hyunwoo@wisenut.co.kr>
2025-07-01 17:00:26 +0900

    test: jwt util test
    - jwt 토큰 생성, 정보 가져오는 로직 테스트

---
+++ src/test/java/com/zizonhyunwoo/anysearch/util/JwtUtilTest.java
@@ -0,0 +1,58 @@
+package com.zizonhyunwoo.anysearch.util;
+
+import com.zizonhyunwoo.anysearch.domain.UserInfo;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class JwtUtilTest {
+
+    String token = null;
+
+    class JwtUtil{
+        String secret= "sadfasdfasdfasfdasafsdfadsfadsfadsfasdfadsfasdfdasdfsdsfs";
+        SecretKey key = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm()
+        );
+
+    }
+
+    @Test
+    @DisplayName("jwt 토큰 생성")
+    public void jwt_토큰_생성(){
+        UserInfo userInfo = new UserInfo("name","password","email","nickname");
+        JwtUtil jwtUtil = new JwtUtil();
+         token = Jwts.builder()
+                .claim("user_name", userInfo.getName())
+                .claim("user_email", userInfo.getEmail())
+                .claim("user_nickname", userInfo.getNickname())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 10000))
+                .signWith(jwtUtil.key)
+                .compact();
+        Claims claims = Jwts
+                .parser()
+                .verifyWith(jwtUtil.key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        Assertions.assertEquals(userInfo.getName(), claims.get("user_name"));
+        Assertions.assertEquals(userInfo.getEmail(), claims.get("user_email"));
+        Assertions.assertEquals(userInfo.getNickname(), claims.get("user_nickname"));
+    }
+
+}
