0fce729c9187e9d5464c15077d237fe63f748f71
hyunwoo <hyunwoo@wisenut.co.kr>
2025-07-01 17:02:00 +0900

    feat: jwt util, filter 추가

---
+++ src/main/java/com/zizonhyunwoo/anysearch/config/JwtFilter.java
@@ -0,0 +1,73 @@
+package com.zizonhyunwoo.anysearch.config;
+
+import com.zizonhyunwoo.anysearch.domain.UserInfo;
+import com.zizonhyunwoo.anysearch.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    protected final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = request.getParameter("Authorization").substring(7);
+
+            checkToken(token);
+            addContext(token);
+
+            filterChain.doFilter(request, response);
+            System.out.println("token check finished");
+        }catch (Exception e) {
+
+            PrintWriter printWriter = response.getWriter();
+            printWriter.print("token error");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
+    private void addContext(String token) {
+        UserInfo userInfo = new UserInfo(
+                jwtUtil.parseJwt(token,"user_name"),
+                "",
+                jwtUtil.parseJwt(token,"user_email"),
+                jwtUtil.parseJwt(token,"user_nickname"),
+                jwtUtil.parseJwt(token,"user_role")
+        );
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                userInfo,
+                null,
+                List.of(
+                        (GrantedAuthority) userInfo::getRole
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void checkToken(String token) throws ServletException {
+        if (!jwtUtil.isExpired(token) &
+            jwtUtil.parseJwt(token,"type").equals("access")) {
+
+            throw new ServletException("access token error");
+        }
+    }
+}
---
+++ src/main/java/com/zizonhyunwoo/anysearch/util/JwtUtil.java
@@ -0,0 +1,57 @@
+package com.zizonhyunwoo.anysearch.util;
+
+import com.zizonhyunwoo.anysearch.domain.UserInfo;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    SecretKey key;
+    @Value("${jwt.key}")
+    String jwtKey;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtKey.getBytes());
+    }
+
+    public String createJwt(UserInfo userInfo, String type){
+        return "Bearer "+Jwts.builder()
+                .claim("user_name", userInfo.getName())
+                .claim("user_email", userInfo.getEmail())
+                .claim("user_nickname", userInfo.getNickname())
+                .claim("user_role", userInfo.getRole())
+                .claim("type", type)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 10000))
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims parseJwt(String jwt){
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
+    }
+
+    public String parseJwt(String jwt, String field){
+        return parseJwt(jwt).get(field, String.class);
+    }
+
+    public Boolean isExpired(String jwt){
+        return parseJwt(jwt).getExpiration()
+                .before(new Date(System.currentTimeMillis()));
+    }
+
+}
