9615b45b04f9a9fdce41f3644297273999d0d76e
hyunwoo <daersh@naver.com>
2025-08-04 17:47:09 +0900

    fix: naver 데이터 초기화 추가
    - job 시작 시 기존 데이터 제거 작업 진행

--- src/main/java/com/zizonhyunwoo/anysearch/batch/jobs/NaverShopJob.java
+++ src/main/java/com/zizonhyunwoo/anysearch/batch/jobs/NaverShopJob.java
@@ -5,17 +5,22 @@ import com.zizonhyunwoo.anysearch.batch.process.NaverProcessor;
 import com.zizonhyunwoo.anysearch.elastic.index.NaverData;
 import com.zizonhyunwoo.anysearch.batch.reader.NaverReader;
 import com.zizonhyunwoo.anysearch.batch.writer.NaverWriter;
+import com.zizonhyunwoo.anysearch.elastic.util.ElasticsearchIndexer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,14 +31,16 @@ public class NaverShopJob {
     private final NaverReader naverReader;
     private final NaverProcessor naverProcessor;
     private final NaverWriter naverWriter;
+    private final ElasticsearchIndexer elasticsearchIndexer;
 
     @Bean
     public Job naverJob(){
+        elasticsearchIndexer.delete("naver");
+
         return new JobBuilder("naverJob",jobRepository)
                 .start(getNaverDatas())
                 .build();
     }
-
 
     private Step getNaverDatas() {
         return new StepBuilder("getNaverDatasStep", jobRepository)
@@ -45,6 +52,5 @@ public class NaverShopJob {
                 .writer(naverWriter)
                 .build();
     }
-
 
 }
